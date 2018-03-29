5a2766165a9c0dd3f2479292c4effb48d45ccb4d
Bannermanj <bannermanj11@yahoo.com>
2018-03-29 17:14:27 -0400

    update app serializers and views

--- predictor_ap/views.py
+++ predictor_ap/views.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from . import models
 from . import serializers
 
-# Create your views here.
-
-class ListTeam(generics.ListCreateAPIView):
-    queryset = models.Team.objects.all()
-    serializer_class = serializers.TeamSerializer
-
-class DetailTeam(generics.RetrieveUpdateDestroyAPIView):
+class TeamViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
     queryset = models.Team.objects.all()
     serializer_class = serializers.TeamSerializer
