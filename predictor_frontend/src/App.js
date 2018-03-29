d4438e488bfc78b555979112e293d1476a464f2f
Bannermanj <bannermanj11@yahoo.com>
2018-03-29 17:17:38 -0400

    add react frontend with map over json

--- predictor_frontend/src/App.js
+++ predictor_frontend/src/App.js
@@ -2,17 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+    {
+        "id": 1,
+        "name": "Tottenham Hotspur",
+        "location": "London, UK",
+        "league": "Premier League"
+    },
+    {
+        "id": 2,
+        "name": "Barcelona",
+        "location": "Barcelona, ES",
+        "league": "La Liga"
+    },
+    {
+        "id": 3,
+        "name": "Liverpool",
+        "location": "Liverpool, UK",
+        "league": "Premier League"
+    },
+    {
+        "id": 4,
+        "name": "Manchester United",
+        "location": "Manchester, UK",
+        "league": "Premier League"
+    },
+    {
+        "id": 5,
+        "name": "Manchester City",
+        "location": "Manchester, UK",
+        "league": "Premier League"
+    }
+]
+
 class App extends Component {
+  state = {
+   teams: []
+ };
+
+ async componentDidMount() {
+   try {
+     const res = await fetch('http://127.0.0.1:8000/api/');
+     const teams = await res.json();
+     this.setState({
+       teams
+     });
+   } catch (e) {
+     console.log(e);
+   }
+ }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.state.teams.map(item => (
+          <div>
+            <h1>{item.name}</h1>
+            <p>{item.location}</p>
+            <p>{item.league}</p>
+          </div>
+        ))}
       </div>
     );
   }
