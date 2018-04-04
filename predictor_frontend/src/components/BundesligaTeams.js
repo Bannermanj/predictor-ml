f385ee1c3336f15151fc9ee96d8ad563ade6801c
Bannermanj <bannermanj11@yahoo.com>
2018-04-03 23:22:45 -0400

    add single league components, add react router to package.json

---
+++ predictor_frontend/src/components/BundesligaTeams.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+
+
+class BundesligaTeams extends Component {
+  state = {
+   bundesligateams: [],
+ };
+
+ async componentDidMount() {
+   try {
+     const res = await fetch('http://127.0.0.1:8000/bundesliga/');
+     const bundesligateams = await res.json();
+     this.setState({
+       bundesligateams,
+     });
+   } catch (e) {
+     console.log(e);
+   }
+ }
+
+  render() {
+    return (
+      <div>
+          <h2> Bundesliga Teams </h2>
+          {this.state.bundesligateams.map(item => (
+            <div>
+              <h3>{item.name}</h3>
+              <p>{item.location}</p>
+              <p>{item.league}</p>
+            </div>
+          ))}
+      </div>
+    );
+  }
+}
+
+export default BundesligaTeams;
